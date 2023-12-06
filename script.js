e1cfb8787291c0fc1b6082de7d33687cefcf0a82
Vaaneesh <vaaneesh1483.be21@chitkara.edu.in>
2023-12-06 17:18:14 +0530

    changes made in script.js and html file

---
+++ script.js
@@ -0,0 +1,25 @@
+const passwordbox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const num = "0123456789";
+const symbol = "!@#$^&*()_+=-[]{}|~";
+const allchars=upperCase+lowerCase+symbol+num;
+
+function createPass(){
+    let password="";
+    password+=upperCase[Math.floor(Math.random()*upperCase.length)];
+    password+=lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    password+=num[Math.floor(Math.random()*num.length)];
+    password+=symbol[Math.floor(Math.random()*symbol.length)];
+
+    while(length>password.length){
+        password+=allchars[Math.floor(Math.random()*allchars.length)];   
+    }
+    passwordbox.value=password;
+}
+function copyPass(){
+    passwordbox.select();
+    navigator.clipboard.writeText(passwordbox.value);
+}
